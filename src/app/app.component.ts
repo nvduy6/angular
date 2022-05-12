64a126a1edd97af026ef64dafc1da82eb7bd7fc3
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-05-12 12:38:43 +0700

    listproduct

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  productName: string = "ProductA";
+  productPrice: number = 10;
+  productStatus: boolean = false;
+  productInfo: {
+    id: number, price: number, name: string
+  } = {
+      id: 1,
+      name: "product A",
+      price: 200
+    };
+  productList: {
+    id: number, price: number, name: string, status: boolean
+  }[] = [
+      {
+        id: 1,
+        name: "productA",
+        price: 200,
+        status: false
+      },
+      {
+        id: 2,
+        name: "productB",
+        price: 300,
+        status: true
+      }
+    ]
 }
