3b7e61cc49a15db43fbc41d183cda1f5da2209d6
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-06-09 12:13:23 +0700

    remove_user

--- src/app/components/layoutAdmin/authen/list-user/list-user.component.ts
+++ src/app/components/layoutAdmin/authen/list-user/list-user.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IpUser } from 'src/app/models/authen';
+import { AuthenService } from 'src/app/services/authen.service';
 @Component({
   selector: 'app-list-user',
   templateUrl: './list-user.component.html',
   styleUrls: ['./list-user.component.css']
 })
 export class ListUserComponent implements OnInit {
-
-  constructor() { }
+userList!:IpUser[];
+  constructor(
+    private authenServie:AuthenService
+  ) { 
+    this.ShowUser()
+  }
 
   ngOnInit(): void {
   }
-
+  ShowUser(){this.authenServie.getUsers().subscribe(data=>{
+    this.userList=data
+  })}
+onRemoveItem(id:number){
+  const confirm = window.confirm('ban co muon xoa khong');
+  if(confirm){
+    this.authenServie.removeUser(id).subscribe(()=>{
+      this.userList=this.userList.filter(item=>item.id!==id)
+    })
+  }
+}
 }
