c59020cc42ae12413fbdcad5368e3dd9f985afda
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-06-11 13:27:45 +0700

    add_user

--- src/app/components/layoutAdmin/authen/add-user/add-user.component.ts
+++ src/app/components/layoutAdmin/authen/add-user/add-user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IpUser } from 'src/app/models/authen';
 import { AuthenService } from 'src/app/services/authen.service';
+import axios, { Axios } from 'axios';
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
@@ -33,20 +34,46 @@ user:IpUser={
     const id = +this.route.snapshot.paramMap.get('id')!;
     // update
     if(id){
-      this.userService.updateUser(this.user).subscribe((data)=>{
-        setTimeout(()=>{
-          this.router.navigate(['/admin/user'])
-        },2000)
+      const apiUr="https://api.cloudinary.com/v1_1/duynv/image/upload";
+      const formdata = new FormData();
+      formdata.append('file',this.user.image);
+      formdata.append("upload_preset","okwdgnez");
+      axios.post(apiUr,formdata,{
+        headers:{
+          "Content-Type": "application/form-data"
+        },
+  
+      }).then((response:any)=>{
+        this.user.image=response.data.url;
+        this.userService.updateUser(this.user).subscribe(data=>{
+          setTimeout(()=>{
+            this.router.navigate(['/admin/post'])
+          })
+        })
       })
     }else{
-      this.userService.addUser(this.user).subscribe(data=>{
-        setTimeout(()=>{
-          this.router.navigate(['admin/user'])
-        },2000)
+      const apiUr="https://api.cloudinary.com/v1_1/duynv/image/upload";
+      const formdata = new FormData();
+      formdata.append('file',this.user.image);
+      formdata.append("upload_preset","okwdgnez");
+      axios.post(apiUr,formdata,{
+        headers:{
+          "Content-Type": "application/form-data"
+        },
+  
+      }).then((response:any)=>{
+        this.user.image=response.data.url;
+        this.userService.addUser(this.user).subscribe(data=>{
+          setTimeout(()=>{
+            this.router.navigate(['/admin/post'])
+          })
+        })
       })
     }
     // add 
    
   }
-
+  changeImage(e:any){
+    this.user.image=e.target.files[0];
+  }
 }
