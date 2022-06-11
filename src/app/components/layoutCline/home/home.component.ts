1fb1828c297777df6b53dfb044795165cb387b82
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-06-11 13:33:58 +0700

    cline_user

--- src/app/components/layoutCline/home/home.component.ts
+++ src/app/components/layoutCline/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IpUser } from 'src/app/models/authen';
+import { AuthenService } from 'src/app/services/authen.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  userList!:IpUser[];
+  constructor(
+    private authenServie:AuthenService
+  ) { 
+    this.ShowUser()
+  }
 
   ngOnInit(): void {
   }
+  ShowUser(){this.authenServie.getUsers().subscribe(data=>{
+    this.userList=data
+  })}
+onRemoveItem(id:number){
+  const confirm = window.confirm('ban co muon xoa khong');
+  if(confirm){
+    this.authenServie.removeUser(id).subscribe(()=>{
+      this.userList=this.userList.filter(item=>item.id!==id)
+    })
+  }
+}
 
 }
